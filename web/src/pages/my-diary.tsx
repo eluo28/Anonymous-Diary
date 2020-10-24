9e5f0a8ae9edfa000ce26af3cdc70f3350cd48ae
eluo28 <edwinluo28@gmail.com>
2020-10-24 14:35:50 -0400

    started creating column

--- web/src/pages/my-diary.tsx
+++ web/src/pages/my-diary.tsx
@@ -1,9 +1,10 @@
+import { Box } from "@chakra-ui/core";
 import { withUrqlClient } from "next-urql";
 import React from "react";
 import { createUrqlClient } from "../utils/createUrqlClient";
 
 export const MyDiary: React.FC<{}> = ({}) => {
-  return <div>Test</div>;
+  return <Box height="100">column</Box>;
 };
 
 export default withUrqlClient(createUrqlClient)(MyDiary);
